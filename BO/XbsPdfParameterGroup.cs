29b2eec80f573de85f7c69a230a5bc1cf9712253
invensoft <38125313+invensoft@users.noreply.github.com>
2018-04-06 16:04:45 +0530

    Add files via upload

---
+++ BO/XbsPdfParameterGroup.cs
@@ -0,0 +1,12 @@
+﻿namespace XBSPDF.BO
+{
+    using System.Collections.Generic;
+
+    class XbsPdfParameterGroup
+    {
+        public string WarrantNumber { get; set; }
+        public string PdfFileName { get; set; }
+        public List<int> PageNumbers { get; set; }
+        public string DelivaryLocation { get; set; }
+    }
+}
